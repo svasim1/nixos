cf605779fb5bf56da63017965908c41b6f0e3b9a
svasim1 <svante.ericsson06@gmail.com>
2026-07-09 20:37:05 +0200

    Firefox configuration

--- home/svante/firefox/default.nix
+++ home/svante/firefox/default.nix
@@ -2,6 +2,41 @@
   programs.firefox = {
     enable = true;
 
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisplayBookmarksToolbar = "always";
+      DontCheckDefaultBrowser = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "extension@tabliss-maintained" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/tablissng/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+
     profiles.default = {
       id = 0;
       name = "Default";
@@ -22,6 +57,18 @@
 
         # Open previous tabs on startup
         "browser.startup.page" = 3;
+
+        # Privacy
+        "browser.contentblocking.category" = "strict";
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.cryptomining.enabled" = true;
+        "datareporting.healthreport.uploadEnabled" = false;
+
+        # Remove clutter
+        "extensions.pocket.enabled" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = false;
+        "browser.urlbar.suggest.searches" = false;
       };
     };
   };
