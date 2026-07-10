1af5ef079a97e75e0e94d2a5008e3b131bccd9c9
svasim1 <svante.ericsson06@gmail.com>
2026-07-10 21:47:38 +0200

    added ssh config

--- home/svante/ssh/default.nix
+++ home/svante/ssh/default.nix
@@ -6,7 +6,15 @@
       "*".identityFile = "~/.ssh/id_ed25519";
     };
     matchBlocks = {
-      # placeholder
+      "ananas.h.krlsg.se ananas" = {
+        hostname = "ananas.h.krlsg.se";
+        user = "svante";
+      };
+      "papaya.h.krlsg.se papaya" = {
+        hostname = "emma-server";
+        user = "svante";
+        proxyJump = "ananas";
+      };
     };
   };
 }
