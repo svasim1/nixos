46078de7113022ab3225fda3cb433ff129151d85
svasim1 <svante.ericsson06@gmail.com>
2026-07-11 13:30:07 +0200

    added AMD gpu configuration for dekstop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -5,6 +5,18 @@
 
   networking.hostName = "svante-nixos";
 
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.libinput.mouse = {
+    accelProfile = "flat";
+    accelSpeed = "-0.20";
+  };
+
   # Early GPU init
   boot.initrd.kernelModules = [ "amdgpu" ];
 }
