3700db2226f5db2105c5769e8d677a72de290051
svasim1 <svante.ericsson06@gmail.com>
2026-07-11 13:30:49 +0200

    removed duplicate mouse sensetivity configuration

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,11 +3,6 @@
   services.displayManager.plasma-login-manager.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [ konsole kwalletmanager ];
 
-  services.libinput.mouse = {
-    accelProfile = "flat";
-    accelSpeed = "-0.20";
-  };
-
   environment.sessionVariables = {
     ELECTRON_FORCE_DARK_MODE = "1";
   };
