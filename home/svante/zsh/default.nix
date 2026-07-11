1484c973f0ec490464edaf5514c2dbb8265778e1
svasim1 <svante.ericsson06@gmail.com>
2026-07-11 17:30:25 +0200

    added nixclean alias

--- home/svante/zsh/default.nix
+++ home/svante/zsh/default.nix
@@ -6,6 +6,7 @@
     initContent = "PROMPT='%F{green}[%n@%m%f:%~]$ '";
     shellAliases = {
       rebuild = "sudo nixos-rebuild switch --flake /home/svante/nixos";
+      nixclean = "read -q \"?Delete all but 5 newest generations? [y/N] \" && echo && sudo nix-env --delete-generations +5 --profile /nix/var/nix/profiles/system && sudo nix-collect-garbage";
     };
   };
 }
